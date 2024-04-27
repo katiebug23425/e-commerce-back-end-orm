0e8e95d4f5501eb576020d14313751036712ea1f
Katie Bug <katierosealford@gmail.com>
2024-04-27 05:30:00 -0400

    defined producttag.js models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,27 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define columns for product_tag table
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
