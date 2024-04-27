b7e4053b16423287131a4ba3dcddd6bc1a4e91c5
Katie Bug <katierosealford@gmail.com>
2024-04-27 06:12:20 -0400

    created category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,53 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({ include: [{model: Product}] });
+    return res.status(200).json(categories);
+  } catch (err) {
+    return res.status(500).json({ message: "Request unable to be fulfilled, not found!"})
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, { include: [{ model: Product}] });
+     if (!category) {
+      return res.status(404).json({ message: "Request unable to be fulfilled, id not found!"})
+     }
+     return res.status(200).json(category);
+  } catch (err) {
+    return res.status(500).json({ message: "Request unable to be fulfilled, not found!"})
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const createdCategory = await Category.create(req.body);
+    return res.status(200).json(createdCategory);
+  } catch (err) {
+    return res.status(400).json({ message: "Request unable to be fulfilled, Category creation failed!"});
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const [updated] = await Category.update(req.body, { where: { id: req.params.id } });
+    !updated ? res.status(404).json({ message: "Request unable to be fulfilled, id not found!" }) : res.status(200).json(updated);
+  } catch (err) {
+    return res.status(500).json({ message: "Request unable to be fulfilled, Category update failed!" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const [deleted] = await Category.destroy({ where: { id: req.params.id} });
+    !deleted ? res.status(404).json({ message: "Request unable to be fulfilled, id not found!" }) : res.status(200).json(deleted);
+  } catch (err) {
+    return res.status(500).json({ message: "Request unable to be fulfilled, Category deletion failed!" });
+  }
 });
+
 
 module.exports = router;
